1305926636ffe234c97c8efe7f8192a1efcf3810
Kahe Mikáyas <132231345+Kahe-Mikaya@users.noreply.github.com>
2024-08-14 11:56:23 +0000

    modificando o dao

--- src/main/java/dao/SupermercadoDao.java
+++ src/main/java/dao/SupermercadoDao.java
@@ -1,11 +1,26 @@
 package dao;
 import model.Supermecado;
-import java.util.Set;
+import java.util.*;
   public class SupermercadoDao{
     private Set<Supermecado> supermercados;
 
     public SupermercadoDao(){
       supermercados = new HashSet<>();
+    }
+    public boolean adicionarSupermercado(Supermecado supermercado){
+      return supermercados.add(supermercado);
+    }
+    public boolean removeSupermercado(Supermecado supermercado){
+      return supermercados.remove(supermercado);
+    }
+    public Supermecado buscarPorCnpj(String cnpj){
+      for(Supermecado supermercado : supermercados){
+        if(supermercado.getCnpj().equals(cnpj)){
+          return supermercado;
+        }
+      }
+      return null;
+      
     }
     
   }
